5cf140626ccee8b2c9cdea7e1c68da1e86ef5ebb
stephenkridel <50998459+stephenkridel@users.noreply.github.com>
2022-05-26 18:22:36 -0400

    Add run command comment

--- cmake-win.sh
+++ cmake-win.sh
@@ -1,4 +1,5 @@
 #!/bin/bash; C:\Program Files\Git\usr\bin\sh.exe
 # Use to help create cmake projects in windows
 #   Input the source dir and build dir as arguments
+#   Run mingw32-make in the build folder instead of "make" to build project
 cmake.exe -G "MinGW Makefiles" -S $1 -B $2
